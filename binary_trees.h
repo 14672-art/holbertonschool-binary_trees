3bd3fb8dd65c2c7da87dcca91b0c340649c1158a
Bilal <14672@holbertonstudents.com>
2026-08-10 12:07:42 +0200

    Add binary_tree_delete prototype and implementation

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include "stddef.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include 
+#include 
+#include 
 
 struct binary_tree_s
 {
@@ -21,5 +21,7 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
